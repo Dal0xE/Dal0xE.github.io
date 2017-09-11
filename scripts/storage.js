c289294b3f6433f05de25ae65ee34a98b8a0cb59
Dal0xE <d.crrenshaw21@gmail.com>
2017-09-11 17:38:51 -0400

    Create storage.js

---
+++ scripts/storage.js
@@ -0,0 +1,49 @@
+window.fakeStorage = {
+  _data: {},
+
+  setItem: function (id, val) {
+    return this._data[id] = String(val);
+  },
+
+  getItem: function (id) {
+    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+  },
+
+  removeItem: function (id) {
+    return delete this._data[id];
+  },
+
+  clear: function () {
+    return this._data = {};
+  }
+};
+
+function LocalStorageManager() {
+  var supported = this.localStorageSupported();
+  this.storage = supported ? window.localStorage : window.fakeStorage;
+}
+
+LocalStorageManager.prototype.localStorageSupported = function () {
+  var testKey = "test";
+  var storage = window.localStorage;
+
+  try {
+    storage.setItem(testKey, "1");
+    storage.removeItem(testKey);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+//Basic setter function. Essentially just a thin wrapper around the internal storage's setItem function.
+LocalStorageManager.prototype.store = function(key, value) {
+  this.storage.setItem(key, value);
+}
+//Basic getter function. Just a wrapper around the internal storage's getItem function.
+LocalStorageManager.prototype.get = function(key) {
+  return this.storage.getItem(key);
+}
+//Returns an item if it exists, 0 otherwise. Good for use when you aren't sure an item exists.
+LocalStorageManager.prototype.seek = function(key) {
+  return this.storage.getItem(key) || 0;
+}
