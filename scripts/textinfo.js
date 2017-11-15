02cdea180d03b5ca48427bbb85cb8cfd06d0534d
Dal0xE <d.crrenshaw21@gmail.com>
2017-11-14 20:18:05 -0500

    Update textinfo.js

--- scripts/textinfo.js
+++ scripts/textinfo.js
@@ -1,6 +1,6 @@
 function getWordCount(text) {
-    a = text.split(' ');
-    b = []
+    var a = text.split(' ');
+    var b = [];
     for (var x; x < a.length; x++) {
         if (a[x].length > 0) {
             b.push(a[x]);
@@ -9,4 +9,34 @@ function getWordCount(text) {
             continue;
         }
     }
+    return (b.length);
+}
+function getAverageWordLength(text) {
+    text = text.split('.').join().split(',').join().split('?').join().split('!').join();
+    text = text.split("'").join().split('/').join(' ').split('(').join().split(')').join();
+    var a = text.split(' ');
+    var b = [];
+    for (var x; x < a.length; x++) {
+        if (a[x].length > 0) {
+            b.push(a[x]);
+        }
+        else {
+            continue;
+        }
+    }
+    var res = 0;
+    for (var x; x < b.length; x++) {
+        res += b[x].length;
+    }
+    return (res / b.length);
+}
+function getNumberOfSentences(text) {
+    var a = text.split('.');
+    var b = [];
+    for (var x; x < a.length; x++) {
+        if (a[x].length > 0) {
+            b.push(a[x]);
+        }
+    }
+    return (b.length);
 }
