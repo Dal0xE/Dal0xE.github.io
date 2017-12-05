f85f62839fc2314bd082689ea75516835a37ef78
Dal0xE <d.crrenshaw21@gmail.com>
2017-12-05 07:54:53 -0500

    Update studymeMain.js

--- scripts/studymeMain.js
+++ scripts/studymeMain.js
@@ -1,1 +1,16 @@
+//Requires LocalStorageManager
+//Requires LocalSet
 
+var currentSet = new localSet();
+
+function actuate(qno) {
+  var q = currentSet.fetch(qno);
+  var type = q.type
+  if (type) {
+    
+
+var allHashData = location.hash.split("#").join();
+var currentMode = allHashData.split("/")[0];
+var currentMode = JSON.parse(currentMode);
+if (currentMode) {
+  
